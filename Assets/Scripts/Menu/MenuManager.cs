0cdf0bfc7f44cbcff783811f5c8cc901a4008945
jeroenrietveld <jeroen_rietveld13@msn.com>
2013-11-15 14:41:31 +0100

    added menu manager

---
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+	public GameObject camera;
+
+	// Use this for initialization
+	void Start () {
+		camera = GameObject.FindGameObjectWithTag("MainCamera");
+
+		camera.transform.position = Vector3.MoveTowards(camera.transform.position, new Vector3(10, 10, 10), Time.deltaTime * 10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
