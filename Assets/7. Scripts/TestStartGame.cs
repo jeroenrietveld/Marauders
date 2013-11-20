dc94e2aa4ccce060be9d6451fe3ac76e77ed38e3
ronaldmeijboom <ronaldme@gmail.com>
2013-11-20 10:48:38 +0100

    Startgame class with a TestClass to run it.

---
+++ Assets/7. Scripts/TestStartGame.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This class is a test to show how the game can be started.
+/// In File -> Build Settings the scene ids are shown. If a certain
+/// map is selected in the menu that id will be given into the 
+/// constructor. So there must be a List or something where
+/// the different scenes are stored.
+/// </summary>
+public class TestStartGame : MonoBehaviour 
+{
+	private StartGame startGame;
+
+	void Awake()
+	{
+		startGame = new StartGame(1);
+	}
+}
