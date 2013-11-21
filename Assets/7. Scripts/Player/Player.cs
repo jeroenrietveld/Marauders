dc39090176e190cdb437893cd5f8bae696370484
jeroenrietveld <jeroen_rietveld13@msn.com>
2013-11-21 11:55:53 +0100

    updated player and input wrapper classes

--- Assets/7. Scripts/Player/Player.cs
+++ Assets/7. Scripts/Player/Player.cs
@@ -4,15 +4,14 @@ using System.Collections;
 public class Player 
 {
 	private int ControllerID;
-	private InputWrapper input;
+	private ControllerMapping Controller;
 	private string Name;
 	private string PrimaryWeapon;
 	private string SecondaryWeapon;
-	private TeamColor Team;
 
 	public Player()
 	{
-
+	
 	}
 
 	void PickUpWeapon(Weapon weapon)
