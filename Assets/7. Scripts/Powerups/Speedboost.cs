37734759475a5548a3ddb079b0a82cdff14de0c8
reb3lzrr <boarderkoen@hotmail.com>
2013-11-25 10:06:36 +0100

    Speedboost

---
+++ Assets/7. Scripts/Powerups/Speedboost.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Speedboost : MonoBehaviour {
+	/// <summary>
+	/// The speed multiplyer
+	/// </summary>
+	public float factor = 1;
+
+	/// <summary>
+	/// The speed additive
+	/// </summary>
+	public float additive = 0;
+
+	/// <summary>
+	/// The duration of the (de)buff in seconds
+	/// </summary>
+	public float duration = 0;
+
+	/// <summary>
+	/// Applys the speedboost
+	/// </summary>
+	/// <param name="inputSpeed">Input speed.</param>
+	public float Apply(float inputSpeed)
+	{
+		return inputSpeed * this.factor + this.additive;
+	}
+
+	void Update()
+	{
+		duration -= Time.deltaTime;
+
+		//Removing the buff
+		if (duration <= 0)
+		{
+			Destroy(this);
+		}
+	}
+
+	void OnEnable()
+	{
+		GetComponent<Player>().movementSpeed.filters += Apply;
+	}
+
+	void OnDisable()
+	{
+		GetComponent<Player>().movementSpeed.filters -= Apply;
+	}
+
+}
