ab61b4c7104d3b7d3b03754b5d73e4a9d0e3ffe9
ronaldmeijboom <ronaldme@gmail.com>
2013-11-26 14:14:09 +0100

    text positions + arrows (prefab created with arrows left/right)

--- Assets/7. Scripts/Menu/ArmoryState.cs
+++ Assets/7. Scripts/Menu/ArmoryState.cs
@@ -4,14 +4,16 @@ using System.Collections.Generic;
 
 public class ArmoryState : MenuStateBase
 {
+    private bool _characterText = false;
+
 	public ArmoryState()
 	{
 		center = GameObject.Find("ArmoryScreen").renderer.bounds.center;
         
 		foreach(GameObject characterSelectBox in GameObject.FindGameObjectsWithTag("CharacterSelect"))
 		{
-			//characterSelectBoxes.Add(CharacterSelectBlock);
-		}
+
+        }
 	}
     
 	public override void Update(MenuManager manager)
@@ -24,6 +26,10 @@ public class ArmoryState : MenuStateBase
 
 	public override void OnActive()
 	{
+        _characterText = true;
+        ArmoryStateText();
+        ArmoryArrows();
+
 		foreach(var block in MonoBehaviour.FindObjectsOfType<CharacterSelectBlock>())
 		{
 			block.enabled = true;
@@ -31,10 +37,34 @@ public class ArmoryState : MenuStateBase
 	}
 
 	public override void OnInactive()
-	{	
+	{
+        _characterText = false;
+        ArmoryStateText();
+        ArmoryArrows();
+
 		foreach(var block in MonoBehaviour.FindObjectsOfType<CharacterSelectBlock>())
 		{
 			block.enabled = false;
 		}
 	}
+
+    private void ArmoryArrows()
+    {
+        GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("MenuArrows");
+
+        foreach (GameObject go in gameObjects)
+        {
+            go.renderer.enabled = _characterText;
+        }
+    }
+
+    private void ArmoryStateText()
+    {
+        GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("MenuText");
+
+        foreach (GameObject go in gameObjects)
+        {
+            go.renderer.enabled = _characterText;
+        }
+    }
 }
