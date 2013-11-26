cefad19d468b3a666c8b80088aa72024cec5a2d3
Joey Koster <koster.joey@gmail.com>
2013-11-26 13:14:43 +0100

    Character selection box update

--- Assets/7. Scripts/Menu/CharacterSelect/JoinGame.cs
+++ Assets/7. Scripts/Menu/CharacterSelect/JoinGame.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using XInputDotNetPure;
 
 /// <summary>
 /// Lets players join the characterscreen selection so they can select a character.
@@ -9,8 +10,14 @@ public class JoinGame : MonoBehaviour
 {
     public List<Material> heroes;
 
-	private Dictionary<ControllerMapping, string> controllers;
+    private IList<string> _heroSelects;
+    public int _playerId;
 	private int _count = 0;
+    private float _defaultTimeValue = 0.15f;
+    private float _timer = 0;
+    private float deadZone = -0.7f;
+    private GamePadState _state;
+    private GameObject _hero;
 
 	// <summary>
 	// Place all the controllers with selection box in the controllers Dictionary.
@@ -18,12 +25,28 @@ public class JoinGame : MonoBehaviour
 	// </summary>
 	void Awake()
 	{
-		controllers = new Dictionary<ControllerMapping, string> ();
-		controllers.Add (InputWrapper.Instance.GetController (1), "hero_select_pl1");
-		controllers.Add (InputWrapper.Instance.GetController (2), "hero_select_pl2");
-		controllers.Add (InputWrapper.Instance.GetController (3), "hero_select_pl3");
-		controllers.Add (InputWrapper.Instance.GetController (4), "hero_select_pl4");
+        _heroSelects = new List<string>();
+        _heroSelects.Add("hero_select_pl1");
+        _heroSelects.Add("hero_select_pl2");
+        _heroSelects.Add("hero_select_pl3");
+        _heroSelects.Add("hero_select_pl4");
+
+        _hero = GameObject.Find(_heroSelects[_playerId - 1]).transform.GetChild(0).gameObject;
+        setGamePad();
+        if(_state.IsConnected)
+        {
+            _hero.renderer.enabled = true;
+            _hero.renderer.material = heroes[_count];
+        }
 	}
+
+    private void setGamePad()
+    {
+        if (_playerId == 1) _state = GamePad.GetState(PlayerIndex.One);
+        else if (_playerId == 2) _state = GamePad.GetState(PlayerIndex.Two);
+        else if (_playerId == 3) _state = GamePad.GetState(PlayerIndex.Three);
+        else if (_playerId == 4) _state = GamePad.GetState(PlayerIndex.Four);
+    }
 
     /// <summary>
     ///  Check every frame if a players wants to join the game. If they press their "A" button
@@ -32,20 +55,44 @@ public class JoinGame : MonoBehaviour
     /// </summary>
     void Update()
     {
-        foreach(KeyValuePair<ControllerMapping, string> pair in controllers)
-		{
-			if(pair.Key.GetButtonDown(XboxButton.A))
-			{
-				GameObject hero = GameObject.Find (pair.Value).transform.GetChild(0).gameObject;
-				hero.renderer.enabled = true;
-				hero.renderer.material = heroes[_count];
-				_count++;
+        setGamePad();
+        float x = _state.ThumbSticks.Left.X;
+        if ((x < deadZone || x > Mathf.Abs(deadZone)) && GetTimer())
+        {
+            if (x > Mathf.Abs(deadZone))
+            {
+                _count++;
+            }
+            else if(x < deadZone)
+            {
+                _count--;
+            }
+            
+            if (_count >= heroes.Count)
+            {
+                _count = 0;
+            }
+            else if (_count < 0)
+            {
+                _count = heroes.Count - 1;
+            }
+            _hero.renderer.material = heroes[_count];
+        }
+    }
 
-				if(_count >= heroes.Count)
-				{
-					_count = 0;
-				}
-			}
-		}
+    /// <summary>
+    /// Runs a timer and returns true wether the user can select the next character.
+    /// </summary>
+    /// <returns></returns>
+    private bool GetTimer()
+    {
+        _timer -= Time.deltaTime;
+
+        if (_timer <= 0f)
+        {
+            _timer = _defaultTimeValue;
+            return true;
+        }
+        return false;
     }
 }
