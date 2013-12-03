8fda87693fe6501ce22143b9a5f4374c0950e61d
Joey Koster <koster.joey@gmail.com>
2013-12-03 12:23:15 +0100

    Fixed bugs in CharacterSelectBlock

--- Assets/7. Scripts/Menu/CharacterSelect/CharacterSelectState.cs
+++ Assets/7. Scripts/Menu/CharacterSelect/CharacterSelectState.cs
@@ -15,18 +15,19 @@ public class CharacterSelectState : SelectionBase
 		if (controller.Axis(Axis.LeftHorizantal) != 0 || controller.JustPressed(Button.DPadLeft) || controller.JustPressed(Button.DPadRight))
         {
 			int direction = 0;
-
+            bool timer = false;
 			if (controller.Axis(Axis.LeftHorizantal) != 0)
 			{
-				direction = (int) controller.Axis(Axis.LeftHorizantal);
-			}
+				direction = controller.Axis(Axis.LeftHorizantal) > 0 ? 1 : -1;
+                timer = true;
+            }
 			else
 			{
 				direction = (-1 * Convert.ToInt32(controller.JustPressed(Button.DPadLeft))) + Convert.ToInt32(controller.JustPressed(Button.DPadRight));
-			}
+            }
 
 			//Selecting new marauder
-			block.changeMarauder(GetMarauderIndex(block.marauderIndex, direction, block.marauders.Count));
+			block.changeMarauder(GetMarauderIndex(block.marauderIndex, direction, block.marauders.Count, timer));
         }
 
         if(controller.JustPressed(Button.A))
@@ -35,12 +36,13 @@ public class CharacterSelectState : SelectionBase
         }
         else if (controller.JustPressed(Button.B))
         {
-            block.OnLeave("Press Start to join");
+            block.OnLeave("Press A to join");
         }
     }
 
     public override void OnActive()
     {
+        block.textHolder.gameObject.SetActive(true);
         block.bigCharacterSelectPlane.renderer.enabled = true;
         block.bigCharacterSelectPlane.renderer.material = block.marauders[block.marauderIndex];
         block.textHolder.text = "Press A to select";
@@ -48,8 +50,7 @@ public class CharacterSelectState : SelectionBase
 
     public override void OnInActive()
     {
+        block.textHolder.gameObject.SetActive(false);
         block.bigCharacterSelectPlane.renderer.enabled = false;
-        block.skillSelectPlane.SetActive(false);
-        block.smallCharacterSelectPlane.SetActive(false);
     }
 }
