20cb2d2a8c7dc8b919c3261959ab2854bc6d7df4
ReneZwanenburg <renezwanenburg@gmail.com>
2013-12-06 16:11:28 +0100

    Add basic TimeBubble
    - TimeBubble mesh added, simply an inverted unit sphere
    - TimeBubble script added, detects when players exit the Time Bubble
    - PlayerSpawner added, Re-enables the player with a variable delay and
    force to be applied.

---
+++ Assets/7. Scripts/Dynamics/PlayerSpawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSpawner : MonoBehaviour {
+
+	public GameObject player;
+	public float spawnDelay = 1f;
+	public Vector3 exitForce = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		gameObject.AddComponent<CameraTracking>();
+		transform.position = player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		spawnDelay -= Time.deltaTime;
+
+		if(spawnDelay <= 0)
+		{
+			player.SetActive(true);
+			player.rigidbody.velocity = Vector3.zero;
+			player.rigidbody.AddForce(exitForce, ForceMode.Impulse);
+			Destroy(gameObject);
+		}
+	}
+}
