d3b502780fb46ff0ccdd6029e3a618ba126b7779
DionnV <dionnv@gmail.com>
2013-12-09 15:47:52 +0100

    Typo; not should be now in the previous commit.

--- Assets/7. Scripts/GameModes/CaptureTheFlag.cs
+++ Assets/7. Scripts/GameModes/CaptureTheFlag.cs
@@ -6,7 +6,7 @@ public class CaptureTheFlag : GameMode
 {
     public CaptureTheFlag() : base(GameModeID.CaptureTheFlag)
     {
-        List<Cell> additives = new List<Cell>();
-        scoreboard.AddCellList(additives);
+        List<Cell> addition = new List<Cell>();
+        scoreboard.AddCellList(addition);
     }
 }
