f8b9d00c31f4d55e1d7406d68b1d707a01cd5ed0
Jeroen Rietveld <Jeroen_Rietveld13@msn.com>
2013-12-11 12:54:28 +0100

    changed character select

---
+++ Assets/7. Scripts/Menu/CharacterSelect/BaseStep.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using XInputDotNetPure;
+
+public class BaseStep : MonoBehaviour 
+{
+	public GamePad controller;
+	public CharacterSelect characterSelect;
+}
