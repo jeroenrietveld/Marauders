adf51cf74bd2cd17c6dbed9e9f44b1d18d7bfab8
ronaldmeijboom <ronaldme@gmail.com>
2013-12-13 13:57:02 +0100

    Enable progressbar only when in charselect + minor changes.

--- Assets/7. Scripts/Menu/LevelSelect/SettingsBlock.cs
+++ Assets/7. Scripts/Menu/LevelSelect/SettingsBlock.cs
@@ -14,13 +14,13 @@ public class SettingsBlock : LevelSelectionBlockBase
     private int _currentIndex;
 	private GameObject _levelDescription;
     private GUIProgressbar _timeSyncBar;
+    private float _progress;
         
 	public SettingsBlock()
-	{    
+	{
         _levelDescription = GameObject.Find("LevelDescription");
-        GameObject.Find("GUIProgressbar").GetComponent<GUIProgressbar>().enabled = true;
         _timeSyncBar = _levelDescription.transform.FindChild("GUIProgressbar").GetComponent<GUIProgressbar>();
-        _timeSyncBar.max = 50;
+        _timeSyncBar.max = 30;
         _timeSyncBar.pos = _timeSyncBar.transform.position;
         _timeSyncBar.size = new Vector3(200, 0, 40);
 	}
@@ -30,26 +30,31 @@ public class SettingsBlock : LevelSelectionBlockBase
 		if(controller.JustPressed(Button.DPadRight))
 		{
             _timeSyncBar.Add(10);
+            _progress += 1;
 		}
 		if(controller.JustPressed(Button.DPadLeft))
 		{
             _timeSyncBar.Add(-10);
+            _progress -= 1;
 		}
-		if(controller.JustPressed(Button.A))
-		{
-            // TODO set the timeSync in GameManager.Instance.matchsettings.timeSync
-            GameManager.Instance.matchSettings.level = LevelSelectionBlock.current.levelName;
-            GameManager.Instance.matchSettings.timeSync = _timeSyncBar.effective;        
+        if (_progress >= 3f)
+        {
+            if (controller.JustPressed(Button.A))
+            {
+                // TODO set the timeSync in GameManager.Instance.matchsettings.timeSync
+                GameManager.Instance.matchSettings.level = LevelSelectionBlock.current.levelName;
+                GameManager.Instance.matchSettings.timeSync = _timeSyncBar.effective;
 
-            // Get the selected game mode class by using the Activator
-            // Default Normal until new game modes are implemented.
-            // var selectedGameMode = Activator.CreateInstance(null, "Normal");
-            // GameManager.Instance.matchSettings.gameMode = (GameMode)selectedGameMode.Unwrap();    
-            
-            // temporary loading Gaia
-            Application.LoadLevel(2);
-            // GameManager.Instance.Start();
-		}
+                // Get the selected game mode class by using the Activator
+                // Default Normal until new game modes are implemented.
+                // var selectedGameMode = Activator.CreateInstance(null, "Normal");
+                // GameManager.Instance.matchSettings.gameMode = (GameMode)selectedGameMode.Unwrap();    
+
+                // temporary loading Gaia
+                Application.LoadLevel(2);
+                // GameManager.Instance.Start();
+            }
+        }
         if(controller.JustPressed(Button.B))
         {
             LevelSelectionManager.ChangeState(LevelSelectionState.LevelSelection);
