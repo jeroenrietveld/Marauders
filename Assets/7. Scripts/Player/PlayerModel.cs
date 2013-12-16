818d2f1d50065b90680d2180209274dd4101819d
Joey Koster <koster.joey@gmail.com>
2013-12-16 13:02:07 +0100

    Added teamcolor to the playermodel

--- Assets/7. Scripts/Player/PlayerModel.cs
+++ Assets/7. Scripts/Player/PlayerModel.cs
@@ -14,4 +14,23 @@ public struct PlayerModel
     public PlayerIndex index { get; set; }
 
     public string marauder { get; set; }
+
+    public TeamColor color { 
+        get 
+        {
+            switch (index)
+            {
+                case PlayerIndex.One:
+                    return TeamColor.Red;
+                case PlayerIndex.Two:
+                    return TeamColor.Blue;
+                case PlayerIndex.Three:
+                    return TeamColor.Green;
+                case PlayerIndex.Four:
+                    return TeamColor.Purple;
+                default:
+                    return TeamColor.Neutral;
+            }
+        } 
+    }
 }
