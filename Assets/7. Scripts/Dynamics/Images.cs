c7bbfdb02e8153eff6984fdd7635cfa8a6d20c70
Mrnuub <wbosland.prive@gmail.com>
2013-12-16 12:55:19 +0100

    Controller Images

---
+++ Assets/7. Scripts/Dynamics/Images.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using XInputDotNetPure;
+using System;
+
+public static class Images
+{
+	private static IDictionary<Button, Texture> btnImages = new Dictionary<Button, Texture>();
+
+	static Images ()
+	{
+		foreach (Button button in Enum.GetValues(typeof(Button)))
+		{
+			btnImages.Add(button, (Texture)Resources.Load("UI/Buttons/" + button.ToString(), typeof(Texture2D))); 
+		}
+	}
+
+	public static Texture Get (Button button)
+	{
+		return btnImages[button];
+	}
+
+}
