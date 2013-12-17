7510f91f674b9cc7f85c8eff92ecad621c361b91
Harold van Schouwen <Hemlockdevelopment@gmail.com>
2013-12-17 10:04:05 +0100

    Samurai animation polish + 3rd player character in Gaia

--- Assets/7. Scripts/Prototype/WeaponPrototype.cs
+++ Assets/7. Scripts/Prototype/WeaponPrototype.cs
@@ -5,10 +5,13 @@ public class WeaponPrototype : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		GameObject katana = WeaponFactory.create ("Katana");
-		PickupSpawner.SpawnWeapon (katana, new Vector3(0, 20, 0));
+		GameObject katana1 = WeaponFactory.create ("Katana");
+		PickupSpawner.SpawnWeapon (katana1, new Vector3(0, 20, 0));
 
-		GameObject daggers = WeaponFactory.create ("Daggers");
-		PickupSpawner.SpawnWeapon (daggers, new Vector3(0, 20, 25));
+		GameObject daggers1 = WeaponFactory.create ("Daggers");
+		PickupSpawner.SpawnWeapon (daggers1, new Vector3(0, 20, 25));
+
+		GameObject katana2 = WeaponFactory.create ("Katana");
+		PickupSpawner.SpawnWeapon (katana2, new Vector3(-5, 20, 0));
 	}
 }
