1d13e6b71ad4c760955768419032ae93cfa754ef
ronaldmeijboom <ronaldme@gmail.com>
2013-12-17 16:22:49 +0100

    audiosource deleted

--- Assets/7. Scripts/Player/Player.Animator.cs
+++ Assets/7. Scripts/Player/Player.Animator.cs
@@ -90,10 +90,6 @@ public partial class Player : MonoBehaviour
 	{
 		//Should jump
 		animation.Play ("Jump", PlayMode.StopSameLayer);
-        if (!Audioclips[2].isPlaying)
-        {
-            Audioclips[2].Play();
-        }
 	}
 	
 	protected void AnimationMovement(float speed)
@@ -112,7 +108,6 @@ public partial class Player : MonoBehaviour
 			//Idling
 			if (speed < 0.2f)
 			{
-                Audioclips[3].Stop();
 				AnimationIdle();
 				return;
 			}
@@ -127,11 +122,6 @@ public partial class Player : MonoBehaviour
 			//Walking
 			if (speed < 3f)
 			{
-                if (!Audioclips[3].isPlaying)
-                {
-                    Audioclips[0].Stop();
-                    Audioclips[3].Play();
-                }
 				animation.CrossFade("Walk", crossFadeDuration, PlayMode.StopSameLayer);
 				animation["Walk"].speed = speed * 0.7f;
 				
@@ -140,11 +130,6 @@ public partial class Player : MonoBehaviour
 						
 			animation.CrossFade("Run", crossFadeDuration, PlayMode.StopSameLayer);
 			animation["Run"].speed = speed * 0.2f;
-            if (!Audioclips[0].isPlaying)
-            {
-                Audioclips[3].Stop();
-                Audioclips[0].Play();
-            }
 						
 			return;
 		} 
@@ -196,11 +181,6 @@ public partial class Player : MonoBehaviour
 			attackAnimationStart = DateTime.Now;
 
 			animation.CrossFade(attackAnimationName, 0.1f, PlayMode.StopSameLayer);
-
-            if (!Audioclips[1].isPlaying)
-            {
-                Audioclips[1].Play();
-            }
 		}
 	}
 }
