e3c96cc11b7bf7f00bad90710641ce9698402c78
DionnV <dionnv@gmail.com>
2013-12-20 10:14:47 +0100

    Made a start with the loading screen.

---
+++ Assets/7. Scripts/Misc/LoadingScreen.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Collections;
+
+public class LoadingScreen : MonoBehaviour
+{
+        private bool loading = true;
+        public Texture loadingTexture;
+
+        void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+        void Update()
+        {
+            if (Application.isLoadingLevel)
+            {
+                loading = true;
+                Debug.Log("AM LOADING");
+            }
+            else
+            {
+                loading = false;
+            }
+        }
+
+        void OnGUI()
+        {
+            if (loading)
+            {
+                GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), loadingTexture, ScaleMode.StretchToFill);
+            }
+        }
+    }
