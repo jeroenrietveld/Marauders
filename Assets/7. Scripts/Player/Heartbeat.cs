8374dc67437586d368f7d4f41f0b3cca918c90bf
Rene Zwanenburg <renezwanenburg@gmail.com>
2013-12-21 17:43:37 +0100

    Heartbeat positioned at ground level
    Translate the hearbeat indicator so it hovers slightly above ground
    level. Keeps current height in world space if there's nothing below the
    avatar. If avatar falls down, heartbeat moves with avatar.

--- Assets/7. Scripts/Player/Heartbeat.cs
+++ Assets/7. Scripts/Player/Heartbeat.cs
@@ -6,16 +6,38 @@ using System.Collections;
 /// </summary>
 public class Heartbeat : MonoBehaviour {
 	public DecoratableFloat heartbeatSpeed = new DecoratableFloat(90);
+	public float groundOffset = 0.1f;
+	private float _groundHeight;
 
 	private Avatar _avatar;
 
-	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		_avatar = transform.parent.GetComponent<Avatar>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+	void FixedUpdate()
+	{
+		var collisionResult = Physics.RaycastAll(_avatar.transform.position + Vector3.up, Vector3.down);
+
+		float maxHeight = float.NegativeInfinity;
+		foreach (var result in collisionResult)
+		{
+			maxHeight = Mathf.Max(maxHeight, result.point.y);
+		}
+
+		if (!float.IsInfinity (maxHeight))
+		{
+			_groundHeight = maxHeight;
+		}
+	}
+
+	void Update ()
+	{
+		var position = transform.position;
+		position.y = Mathf.Min(_groundHeight, _avatar.transform.position.y) + groundOffset;
+		transform.position = position;
+
 		//Rotate the health circle
 		transform.Rotate(Vector3.up, heartbeatSpeed * Time.deltaTime);
 		//Pass player health to shader
