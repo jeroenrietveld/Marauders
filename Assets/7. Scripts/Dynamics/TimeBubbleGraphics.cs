3ab62f13350ab847b227c52de26b5c7d874025c5
Rene Zwanenburg <renezwanenburg@gmail.com>
2013-12-22 15:12:11 +0100

    Improve time bubble distortion on object enter
    - Animation no longer resets when multiple objects enter simultaneously.
    - Use Timer.

--- Assets/7. Scripts/Dynamics/TimeBubbleGraphics.cs
+++ Assets/7. Scripts/Dynamics/TimeBubbleGraphics.cs
@@ -5,7 +5,8 @@ public class TimeBubbleGraphics : MonoBehaviour {
 
 	private Vector4 _currentOffsets = Vector4.zero;
 	public Vector4 offsetMovementSpeed = Vector4.zero;
-	public Vector4 offsetSpeedExitMultiplier = Vector4.one;
+
+	private Timer _distortionTimer;
 
 	private float secondaryDistortionStartTime;
 	private float secondaryDistortionAnimationTime;
@@ -15,40 +16,28 @@ public class TimeBubbleGraphics : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		Event.register<TimeBubbleEnterEvent> (OnEnterEvent);
-		//Event.register<TimeBubbleObjectExitEvent> (OnExitEvent);
+
+		_distortionTimer = new Timer (1.5f);
+		_distortionTimer.endPhase = 1.5f;
+		_distortionTimer.startPhase = -0.025f;
 
 		_currentOffsets = new Vector4(Random.value, Random.value, Random.value, Random.value);
-		AnimateSecondaryDistortion (1.5f, float.Epsilon);
 	}
 
 	void OnDestroy()
 	{
 		Event.unregister<TimeBubbleEnterEvent> (OnEnterEvent);
-		//Event.unregister<TimeBubbleObjectExitEvent> (OnExitEvent);
 	}
 
 	private void OnEnterEvent(TimeBubbleEnterEvent evt)
 	{
-		AnimateSecondaryDistortion (secondaryDistortionEndValue == -0.025f ? 1.5f : -0.025f , 1.5f);
-		offsetMovementSpeed.Scale(offsetSpeedExitMultiplier);
-		//AnimateSecondaryDistortion (2, 3);
-	}
-
-	private void OnExitEvent(TimeBubbleObjectExitEvent evt)
-	{
-
-	}
-
-	void AnimateSecondaryDistortion(float targetValue, float time)
-	{
-		secondaryDistortionStartTime = Time.time;
-		secondaryDistortionAnimationTime = time;
-		secondaryDistortionStartValue = renderer.material.GetFloat ("DistortionCenter2");
-		secondaryDistortionEndValue = targetValue;
+		if (!_distortionTimer.running) _distortionTimer.Start ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		_distortionTimer.Update ();
+
 		var movement = Vector4.Scale (offsetMovementSpeed, Vector4.one * Time.deltaTime);
 
 		_currentOffsets = new Vector4 (
@@ -60,14 +49,9 @@ public class TimeBubbleGraphics : MonoBehaviour {
 
 		renderer.material.SetVector ("FractalOffsets", _currentOffsets);
 
-
-		float phase = Mathf.Clamp01(
-			(Time.time - secondaryDistortionStartTime) / secondaryDistortionAnimationTime
-		);
-
 		renderer.material.SetFloat (
-			"DistortionCenter2", 
-			Mathf.Lerp(secondaryDistortionStartValue, secondaryDistortionEndValue, phase)
+			"DistortionCenter2",
+			_distortionTimer.Phase()
 		);
 	}
 }
