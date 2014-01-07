2525358771cd5d670693a2b0c48863d5cd25ae10
reb3lzrr <boarderkoen@hotmail.com>
2014-01-07 12:32:50 +0100

    FlipPolygons.cs

---
+++ Assets/7. Scripts/Misc/FlipPolygons.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class FlipPolygons : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake()
+	{
+		Mesh mesh = GetComponent<MeshCollider>().mesh;
+		int[] triangles = mesh.triangles;
+		Array.Reverse(triangles);
+		mesh.triangles = triangles;
+	}
+}
