f2a1baf6f55bb6aa474ae51b64295995abaa3bae
Rene Zwanenburg <renezwanenburg@gmail.com>
2014-01-13 14:42:42 +0100

    Started work on Gaia objective

---
+++ Assets/7. Scripts/Objective/Gaia/Shrine.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public struct ShrineCapturedEvent
+{
+	public Shrine shrine;
+	public Player newOwner;
+	public Player oldOwner;
+
+	public ShrineCapturedEvent(Shrine shrine, Player newOwner, Player oldOwner)
+	{
+		this.shrine = shrine;
+		this.newOwner = newOwner;
+		this.oldOwner = oldOwner;
+	}
+}
+
+public class Shrine : Attackable {
+
+	private bool _canBeCaptured = true;
+	public bool canBeCaptured { get { return _canBeCaptured; } }
+
+	private Player _owner;
+	public Player owner { get { return _owner; } }
+
+
+	public bool captured {  get { return _owner != null; } }
+
+	public override void OnAttack(Attack attacker)
+	{
+		if (_canBeCaptured && attacker.isCombo)
+		{
+			var player = attacker.GetComponent<Avatar>().player;
+
+			if(player != _owner)
+			{
+				var oldOwner = _owner;
+				_owner = player;
+
+				Event.dispatch(new ShrineCapturedEvent(this, player, oldOwner));
+			}
+		}
+	}
+	
+	public void Reset()
+	{
+		_canBeCaptured = true;
+		_owner = null;
+	}
+
+	public void Activate()
+	{
+		_canBeCaptured = true;
+	}
+}
---
+++ Assets/7. Scripts/Objective/Gaia/ShrineManager.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShrineManager : MonoBehaviour {
+
+	public int timeSyncPerShrine;
+	public float activationDelay = 20;
+
+	private Shrine[] _shrines;
+	private Timer _activationTimer;
+
+	// Use this for initialization
+	void Start () {
+		_shrines = GetComponentsInChildren<Shrine> ();
+
+		_activationTimer = new Timer (activationDelay);
+		_activationTimer.AddPhaseCallback (ActivateRandomShrine);
+
+		_activationTimer.Start ();
+	}
+
+	void OnEnable()
+	{
+		Event.register<ShrineCapturedEvent> (OnShrineCaptured);
+	}
+
+	void OnDisable()
+	{
+		Event.unregister<ShrineCapturedEvent> (OnShrineCaptured);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_activationTimer.Update ();
+	}
+
+	void OnShrineCaptured(ShrineCapturedEvent evt)
+	{
+		bool allCaptured = true;
+		foreach(var shrine in _shrines)
+		{
+			allCaptured = allCaptured && shrine.captured;
+		}
+
+		if (allCaptured)
+		{
+			ApplyReward();
+		}
+
+		_activationTimer.Start ();
+	}
+
+	private void ApplyReward()
+	{
+
+
+		foreach (var shrine in _shrines)
+		{
+			shrine.owner.AddTimeSync(timeSyncPerShrine);
+			shrine.Reset();
+		}
+	}
+
+	private void ActivateRandomShrine()
+	{
+		//TODO: make random
+		foreach(var shrine in _shrines)
+		{
+			if(!shrine.canBeCaptured)
+			{
+				shrine.Activate();
+				break;
+			}
+		}
+	}
+}
