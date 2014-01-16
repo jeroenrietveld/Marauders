c0beaf8e8eabec62fb2310cc83df9e2801a62799
reb3lzrr <boarderkoen@hotmail.com>
2014-01-16 13:19:57 +0100

    Fixing score

--- Assets/7. Scripts/GameModes/ScoreHandler.cs
+++ Assets/7. Scripts/GameModes/ScoreHandler.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using UnityEngine;
 using System.Collections;
 
@@ -6,42 +5,6 @@ public class ScoreHandler : MonoBehaviour {
 
 	public int killScore = 5;
 	public int suicidePenalty = -5;
-
-	// Use this for initialization
-	void Start () {
-		Event.register<AvatarDeathEvent>(OnAvaterDeath);
-		Event.register<TimeBubbleAvatarExitEvent>(OnTimeBubbleExit);
-	}
-
-	void OnDisable()
-	{
-		Event.unregister<AvatarDeathEvent> (OnAvaterDeath);
-		Event.unregister<TimeBubbleAvatarExitEvent>(OnTimeBubbleExit);
-	}
-
-	void OnAvaterDeath(AvatarDeathEvent evt)
-	{
-		if(evt.offender != null) 
-		{
-			evt.offender.AddTimeSync (killScore);
-		}
-
-		evt.victim.AddTimeSync (suicidePenalty);
-	}
-
-	void OnTimeBubbleExit(TimeBubbleAvatarExitEvent evt)
-	{
-		evt.avatar.player.AddTimeSync (suicidePenalty);
-	}
-}
-=======
-﻿using UnityEngine;
-using System.Collections;
-
-public class ScoreHandler : MonoBehaviour {
-
-	public int killScore = 50;
-	public int deathPenalty = -25;
 
 	// Use this for initialization
 	void Start () {
@@ -74,4 +37,3 @@ public class ScoreHandler : MonoBehaviour {
         GameManager.scoreboard.AddContent(evt.avatar.player.index, "Time Sync", deathPenalty);
 	}
 }
->>>>>>> b4d2b0af6b6161cfbb2b7bad2a06eb9fe7020777
