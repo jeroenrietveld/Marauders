15abea00765e285ae4f03430e0aa42e17859d8d6
Jeroen Rietveld <Jeroen_Rietveld13@msn.com>
2014-01-21 10:09:51 +0100

    updated game ui and destabilize

--- Assets/7. Scripts/Player/Skills/Destabilize.cs
+++ Assets/7. Scripts/Player/Skills/Destabilize.cs
@@ -5,6 +5,8 @@ public class Destabilize : SkillBase
 {
 	private Timer _destabilize;
 
+	private float _duration = 3f;
+
 	private static AnimationHandler.AnimationSettings _animationSettings = new AnimationHandler.AnimationSettings (
 		//TODO: Use correct animation
 		"Dash",
@@ -13,19 +15,28 @@ public class Destabilize : SkillBase
 		WrapMode.Loop
 		);
 
-	public Destabilize() : base(1, _animationSettings)
+	public Destabilize() : base(5, _animationSettings)
 	{
 		Material mat = (Material)Resources.Load ("Materials/Destabilize");
 
-		_destabilize = new Timer (1f);
+		_destabilize = new Timer (_duration);
 		_destabilize.AddCallback (0f, delegate {
 			GetComponentInChildren<Attackable>().isAttackable = false;
 			GetComponent<AvatarGraphics>().AddMaterial(mat);
+
+			Stun stun = gameObject.AddComponent<Stun>();
+			Destroy (stun, _duration);
+
+			GetComponent<AnimationHandler>().Pause();
+			animation.Stop();
 		});
 
 		_destabilize.AddCallback (_destabilize.endTime, delegate {
 			GetComponentInChildren<Attackable>().isAttackable = true;
 			GetComponent<AvatarGraphics>().RemoveMaterial(mat);
+			
+			GetComponent<AnimationHandler>().UnPause();
+			//animation.Play();
 		});
 	}
 
